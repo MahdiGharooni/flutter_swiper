72a3b6e1d1b9098cdb33466e173e034a372a5c62
MahdiGharooni <39171814+MahdiGharooni@users.noreply.github.com>
2021-11-14 14:50:11 +0330

    migrated to null safety
    migrated to null safety

--- lib/src/swiper_controller.dart
+++ lib/src/swiper_controller.dart
@@ -1,42 +1,43 @@
-import 'package:flutter_swiper/src/swiper_plugin.dart';
-import 'package:transformer_page_view/transformer_page_view.dart';
+import 'package:sun/widgets/flutter_swiper/flutter_swiper.dart';
+import 'package:sun/widgets/transformer_page_view/index_controller.dart';
 
 class SwiperController extends IndexController {
+  SwiperController();
   // Autoplay is started
-  static const int START_AUTOPLAY = 2;
+  static const int  START_AUTOPLAY = 2;
 
   // Autoplay is stopped.
-  static const int STOP_AUTOPLAY = 3;
+  static const int  STOP_AUTOPLAY = 3;
 
   // Indicate that the user is swiping
-  static const int SWIPE = 4;
+  static const int  SWIPE = 4;
 
   // Indicate that the `Swiper` has changed it's index and is building it's ui ,so that the
   // `SwiperPluginConfig` is available.
-  static const int BUILD = 5;
+  static const int  BUILD = 5;
 
   // available when `event` == SwiperController.BUILD
-  SwiperPluginConfig config;
+  SwiperPluginConfig? config;
 
   // available when `event` == SwiperController.SWIPE
   // this value is PageViewController.pos
-  double pos;
+  double? pos;
 
-  int index;
-  bool animation;
-  bool autoplay;
-
-  SwiperController();
+  @override
+  int? index;
+  @override
+  bool? animation;
+  bool? autoplay;
 
   void startAutoplay() {
     event = SwiperController.START_AUTOPLAY;
-    this.autoplay = true;
+    autoplay = true;
     notifyListeners();
   }
 
   void stopAutoplay() {
     event = SwiperController.STOP_AUTOPLAY;
-    this.autoplay = false;
+    autoplay = false;
     notifyListeners();
   }
 }
